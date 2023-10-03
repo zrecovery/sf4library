3955240a7df4a56013033004cb2dd5830424dec6
zrecovery <zrecovery@outlook.com>
2023-10-03 21:28:51 +0800

    chore: 添加smui包

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
-  plugins: [solid()],
+    plugins: [solid(), suidPlugin()],
+    build: {
+        target: "esnext",
+    },
 });
